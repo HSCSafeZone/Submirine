948690c17d44ca9c2383ca60ece63b89eab24146
yeonjinb <155068491+yeonjinb@users.noreply.github.com>
2024-05-24 10:54:58 +0900

    Update SafeZoneClient.java
    5.23일자 수정

--- SafeZoneClient.java
+++ SafeZoneClient.java
@@ -4,10 +4,10 @@ import java.util.*;
 
 class SafeZoneClient {
     static int inPort = 9999;
-    static String address = "localhost";
+    static String address = "172.30.1.78";
     static PrintWriter out;
     static BufferedReader in;
-    static String userName = "Alice";
+    static String userName;
     static int num_mine = 10;
     static int width = 9;
 
@@ -19,15 +19,18 @@ class SafeZoneClient {
         try (Socket socket = new Socket(address, inPort)) {
             out = new PrintWriter(socket.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-            System.out.println("Welcome!");
+            
+            System.out.println("Enter your user name(letters & numbers only)...");
+            Scanner scan = new Scanner(System.in);
+            userName = scan.nextLine();
             out.println(userName);
-            msg = in.readLine(); // wait message
+            msg = in.readLine(); // "Welcome userName!"
             System.out.println(msg);
-            msg = in.readLine(); // start message
-            System.out.println(msg);
-
+//            msg = in.readLine(); // start message
+//            System.out.println(msg);
+            
             while (score <= num_mine) {
+            	//서버에서 "ok"를 내보내면 "enter x coordinate" 뜨게 하기 
                 msg = guess();
                 if (msg.equalsIgnoreCase("ok")) {
                     msg = in.readLine();
@@ -37,7 +40,6 @@ class SafeZoneClient {
                         System.out.println("hit , score = " + score);
                     } else
                         System.out.println("miss , score = " + score);
-
                 }
 
             }
@@ -53,21 +55,35 @@ class SafeZoneClient {
 
     public static String guess() throws IOException {
         Scanner scan = new Scanner(System.in);
-
-        System.out.print("\n Enter x coordinate:");
-        int x = scan.nextInt();
+        int x = -1;
+        int y = -1;
+        
         while ((x < 0) || (x >= width)) {
-            System.out.println(" Invalid x, enter a new x coordinate");
-            x = scan.nextInt();
+        	try {
+		        System.out.print("\n Enter x coordinate: ");
+		        x = scan.nextInt();
+		        if (x < 0 || x >= width) {
+		        	System.out.println("Invalid x, enter a new x coordinate");
+		        }
+        	} catch (InputMismatchException e) {
+	            System.out.println("Invalid input, please enter an integer.");
+	            scan.next(); // Clear the invalid input
+        	}
         }
-        System.out.print(" Enter y coordinate:");
-        int y = scan.nextInt();
         while ((y < 0) || (y >= width)) {
-            System.out.println(" Invalid y, enter a new y coordinate");
-            y = scan.nextInt();
+        	try {
+		        System.out.print(" Enter y coordinate: ");
+		        y = scan.nextInt();
+		        if (y < 0 || y >= width) {
+		        	System.out.println("Invalid y, enter a new y coordinate");
+		        }
+        	} catch (InputMismatchException e) {
+	            System.out.println("Invalid input, please enter an integer.");
+	            scan.next(); // Clear the invalid input
+        	}
         }
 
-        System.out.println("wait for turn");
+        System.out.println("wait for your turn");
         out.println(x + "," + y);
         String msg = in.readLine();
 
